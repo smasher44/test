829509e84d9782481c5917703c92baa8221553ee
smasher44 <georgelibatique819gmail.com>
2022-09-11 14:56:16 +0800

    Deleted some comments

--- index.js
+++ index.js
@@ -29,17 +29,6 @@ import { coop_id_laman } from './constants/apiTestData'
 // import 'react-datetime/css/react-datetime.css';
 // import 'antd/dist/antd.css';
 
-// middlewares
-// const history = createHashHistory()
-// const sagaMiddleware = createSagaMiddleware()
-// const routeMiddleware = routerMiddleware(history)
-// const middlewares = [sagaMiddleware, routeMiddleware]
-// if (process.env.NODE_ENV === 'development') {
-//   middlewares.push(logger)
-// }
-// const store = createStore(reducers(history), compose(applyMiddleware(...middlewares)))
-// axios.defaults.baseURL = "http://api.arcs.ph"
-// axios.defaults.baseURL = "https://api.picpabbmp.com/";
 axios.defaults.baseURL = "https://api.arcs.ph"
 // axios.defaults.headers.common = {
 //     ["for-coop-id"]: coop_id_laman
